665257fc222ef0af3d8250c18c26c59f98bd499f
HLNoll <hyuryluis@gmail.com>
2019-12-01 11:05:56 -0300

    ok

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -7,7 +7,7 @@ const UserController =  {
         UserService.Rules.Login(req.body).then(
             result =>{
                 if(result.status === 404){
-                   return res.status(404)
+                   return res.json(result).status(404)
                 }else{
                    return res.json(result).status(200);
                 }
